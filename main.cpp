9867aa296927290a5ed0238a385896326ca29551
notcoose <markusgperez@gmail.com>
2024-01-31 13:04:19 -0600

    Added off-github work

--- main.cpp
+++ main.cpp
@@ -1,1 +1,123 @@
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// Function prototypes at the top of the file for function decomposition
+
+
+//Print instructions for using the program.
+
+void printMenu();
+/**
+ * Returns the 0-based index in the English alphabet where `c` appears,
+ * or -1 if `c` is not an uppercase letter in the alphabet.
+ *
+ * For example:
+ *  - `findIndexInAlphabet('A')` returns 0
+ *  - `findIndexInAlphabet('D')` returns 3
+ *  - `findIndexInAlphabet('+')` returns -1
+ *  - `findIndexInAlphabet('a')` returns -1
+ */
+int findIndexInAlphabet(char c);
+/**
+ * Returns `c` rotated by `amount` many characters forward. If we run out
+ * of letters in the alphabet, wrap around back to 'A'. This method
+ * assumes that `c` is an uppercase letter.
+ * For example:
+ *  - `rot('A', 0)` returns 'A'
+ *  - `rot('A', 1)` returns 'B'
+ *  - `rot('Z', 1)` returns 'A'
+ *  - `rot('A', 10)` returns 'K'
+ *  - `rot('J', 25)` returns 'I'
+ */
+char rot(char c, int amount);
+/**
+ * Returns a new string in which every character has been rotated by `amount`
+ * letters. Lowercase letters are converted to uppercase and rotated.
+ * Non-alphabetic characters are left alone.
+ * For example:
+ *  `rot("A", 0)` returns "A"
+ *  `rot("AA", 3)` returns "DD"
+ *  `rot("HELLO", 0)` returns "HELLO"
+ *  `rot("HELLO", 1)` returns "IFMMP"
+ *  `rot("oh HEL-LO!", 1)` returns "PI IFM-MP!"
+ */
+string rot(string line, int amount);
+
+int main() {
+    string command;
+
+    cout << "Welcome to Ciphers!" << endl;
+    cout << "-------------------" << endl;
+    cout << endl;
+
+    do {
+        printMenu();
+        cout << endl << "Enter a command (case does not matter): ";
+
+        // Use getline for all user input to avoid needing to handle
+        // input buffer issues relating to using both >> and getline
+        getline(cin, command);
+        cout << endl;
+
+        if(command[0] == 'C' || command[0] == 'c'){
+
+        }
+        else if(command[0] == 'D' || command[0] == 'd'){
+
+        }
+        else if(command[0] == 'V' || command[0] == 'v'){
+
+        }
+
+        cout << endl;
+
+    } while (!(command == "x" || command == "X"));
+
+    return 0;
+}
+
+void printMenu() {
+    cout << "Ciphers Menu" << endl;
+    cout << "------------" << endl;
+    // Caesar Ciphers
+    cout << "C - Encrypt with Caesar Cipher" << endl;
+    cout << "D - Decrypt Caesar Cipher" << endl;
+    // Vigenere
+    cout << "V - Encrypt with Vigenère" << endl;
+    // Utility
+    cout << "X - Exit Program" << endl;
+}
+
+int findIndexInAlphabet(char c) {
+    int index = ALPHABET.find(c);
+
+    if(index != string::npos)
+        return index;
+    else
+        return -1;
+}
+
+char rot(char c, int amount) {
+    int charIndex = findIndexInAlphabet(c);
+
+    if(charIndex != -1)
+        c = ALPHABET.at((findIndexInAlphabet(c) + amount) % 26);
+
+    return c;
+}
+
+string rot(string line, int amount) {
+    string temp = "";
+
+    for(char i: line)
+        temp += toupper(rot(i, amount));
+
+    return temp;
+}
